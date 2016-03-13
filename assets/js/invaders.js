8e8abd330a288aabacbf66436028233c7c0d567a
Dan Peddle <dan@flarework.com>
2016-03-14 00:42:26 +0100

    switch waves when all dead

--- assets/js/invaders.js
+++ assets/js/invaders.js
@@ -11,7 +11,7 @@ const game = $('.game');
 const ctx = game.getContext('2d');
 const WIDTH = 512;
 const HEIGHT = 512;
-
+const GRID_SIZE = 60;
 // const div = document.createElement.bind(document, 'div');
 // const invader = c => {let a = div()}; a.className = 'invader';a.textContent = c; return a};
 // const tick = Bacon.repeatedly(1000, [0,1]);
@@ -26,8 +26,9 @@ function makeButton(text) {
 }
 
 let {btn,stream} = makeButton('tick');
-let tick = Bacon.interval(32);
-// tick = stream;
+let tick = stream;
+tick = Bacon.interval(32);
+
 body.appendChild(btn);
 tick.map('tick')
 
@@ -47,6 +48,7 @@ function bindInputs() {
         right: rights
     }
 }
+bindInputs().left.log();
 
 function clearBoard() {
     ctx.clearRect(0,0,WIDTH, HEIGHT);
@@ -56,7 +58,7 @@ function drawInvader(pos) {
     // if (!pos) return;
     let {x,y} = pos;
     ctx.beginPath();
-    ctx.fillStyle = 'rgba(255,255,255,0.1)';
+    ctx.fillStyle = 'rgba(255,32,32,0.8)';
     ctx.rect(x,y,13,7);
     ctx.rect(x+3,y-3,7,7)
     ctx.fill();
@@ -67,90 +69,77 @@ window.clearBoard = clearBoard;
 window.draw = drawInvader;
 
 
-function getInvaders() {
-    return Bacon.repeat(function() {
-        // infinite stream of blank invaders
-        // this will be scanned by the board function to assemble the wave
-        return Bacon.once({
-            idx: 0,
-            pos: new Victor({x:0, y: 0}),
-            direction: 1
-        });
-    });
+function getInvader(i) {
+
+    let x = (i % 8) + 1;
+    let y = Math.floor(i / 8) + 1;
+
+    let pos = [x,y].map(n => n*GRID_SIZE);
+
+    let v = new Victor(...pos);
+
+    return {
+        idx: 0,
+        pos: v,
+        direction: 1,
+        dead: false
+    };
 
 }
 
-// wave is a proprerty
-// wave updates based in filtering from another property which is the dead invaders
-// tick samples the wave to know which invaders to render
-// when wave is empty, make a new one
 
-function bullet() {
-
+function emptyArray(size) {
+    return Array.apply(null, {length: size});
 }
 
-function invert(n) {
-    return n * -1;
-}
+// function invert(n) {
+//     return n * -1;
+// }
 
-
-function direction() {
-    return Bacon.constant(-1)
-        .take(1)
-        // .flatMap(direction)
-        .map(n => n*-1)
-        .toProperty();
-}
+//
+// function direction() {
+//     return Bacon.constant(-1)
+//         .take(1)
+//         // .flatMap(direction)
+//         .map(n => n*-1)
+//         .toProperty();
+// }
 
 
 function wave() {
-    let waveSize = 32;
-    let x = 1;
-    let y = 1;
-    let GRID_SIZE = 60;
+    console.log('Making new wave!');
 
-    // the stream ends once we take some :(
-    return getInvaders()
-            .take(waveSize)
-            .reduce([], function(acc,i) {
+    let w = emptyArray(32).map( (_,i) => getInvader(i) );
 
-
-                if (x * GRID_SIZE >= (WIDTH - 30) ) {
-                    y++;
-                    x = 1;
-                }
-
-                let Y = y * GRID_SIZE;
-                let X = x * GRID_SIZE;
-
-                x = x + 1;
-
-                let {pos} = i;
-                pos.x = X;
-                pos.y = Y;
-                acc.push(i);
-                return acc;
-            })
-            .filter(function(i) {
-                // remove dead invaders here
-                return true; // return everything for now
-                // oh nuts, this actually is just ever one value. how to map the dead ones with live?
-            })
-            .toProperty()
+    return Bacon.once(w)
+                .sampledBy(tick)
+                .filter(function(v) {
+                    // are they all dead?
+                    return v.reduce(function(acc,i) {
+                        return i.dead && acc;
+                    },true);
+                })
+                .take(1)
+                .flatMapLatest(wave)
+                .toProperty(w);
 }
 
+function updatePosition(i) {
+    i.pos.x += 3 * i.direction;
+    if (i.pos.x >= WIDTH - 30 || i.pos.x <= 30) {
+        i.direction = i.direction * - 1;
+    }
+    // simulate pew pew
+    i.dead = !i.dead ? Math.random() > 0.95 : i.dead;
+    return i;
+}
 
-wave().sampledBy(tick).map(function(w) {
-    return w.map( i => {
-        i.pos.x += 3 * i.direction;
-        if (i.pos.x >= WIDTH - 30 || i.pos.x <= 30) {
-            i.direction = i.direction * - 1;
-        }
-        return i;
+function updatePositions(w) {
+    return w.map(updatePosition);
+}
+
+wave().sampledBy(tick).map(updatePositions)
+    .onValue(function(w) {
+        clearBoard();
+        w.forEach(i => !i.dead && drawInvader(i.pos));
     });
-}).onValue(function(w) {
-    clearBoard();
-    w.forEach(function({pos}) {
-        drawInvader(pos);
-    });
-})
